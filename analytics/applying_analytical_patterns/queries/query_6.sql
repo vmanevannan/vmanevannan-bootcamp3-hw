748017c56268e2fc8cd7d5340ce0ab32c827fb9c
Varuna Manevannan <varuna@Varunas-MacBook-Pro.local>
2023-12-09 17:27:33 -0500

    modified query6

--- analytics/applying_analytical_patterns/queries/query_6.sql
+++ analytics/applying_analytical_patterns/queries/query_6.sql
@@ -1,29 +1,74 @@
-WITH game_details AS
-(
-   SELECT
+WITH
+  game_details AS (
+    SELECT
       gd.game_id,
-      MAX(g.game_date_est) as game_date,
-      MAX(g.home_team_wins) as home_team_wins,
-      MAX(gd.team_id) as team_id,
-      MAX(gd.team_abbreviation) as team_abbreviation
+      MAX(g.game_date_est) AS game_date,
+      MAX(g.home_team_wins) AS home_team_wins,
+      MAX(gd.team_id) AS team_id,
+      MAX(gd.team_abbreviation) AS team_abbreviation
     FROM
-      bootcamp.nba_game_details gd JOIN bootcamp.nba_games g ON gd.game_id = g.game_id
-    GROUP BY gd.game_id
-)
-WITH prev_game_details AS
-(
-SELECT *,
-      ROW_NUMBER() OVER (
-        PARTITION BY team_id
+      bootcamp.nba_game_details gd
+      JOIN bootcamp.nba_games g ON gd.game_id = g.game_id
+    GROUP BY
+      gd.game_id
+  ),
+lagged AS (
+    SELECT
+      *,
+      LAG(home_team_wins,1) OVER (
+        PARTITION BY
+          team_id
         ORDER BY
           game_date
-      ) AS game_number,
-      LAG(home_team_wins,1) OVER(
-      PARTITION BY team_id
-      ORDER BY game_date
-      ) AS prev_game_status
-FROM game_details
-ORDER BY team_id, game_date
+      ) AS did_win_game_before
+    FROM
+      game_details
+    ORDER BY
+      team_id,
+      game_date
+  ),
+streak_changes AS (
+SELECT
+ *,
+  CASE WHEN home_team_wins != did_win_game_before THEN 1 ELSE 0 
+  END as streak_changed
+FROM
+lagged
+),
+streak_identified AS(
+SELECT *,
+SUM(streak_changed) OVER (
+        PARTITION BY
+          team_id
+        ORDER BY
+          game_date
+      ) as streak_identifier
+FROM
+streak_changes),
+
+record_counts AS (
+SELECT *, 
+ROW_NUMBER() OVER (PARTITION BY
+          team_id, streak_identifier
+        ORDER BY
+          game_date
+      ) as streak_length
+FROM streak_identified
+),   
+ranked AS(
+SELECT *,
+RANK() OVER (PARTITION BY
+          team_id, streak_identifier
+        ORDER BY
+          streak_length DESC
+      ) as rank
+FROM record_counts
 )
-SELECT *
-FROM prev_game_details 
+
+SELECT team_id, 
+       MAX(team_abbreviation),
+       MAX(streak_length) AS longest_streak
+FROM ranked
+GROUP BY team_id
+ORDER BY MAX(streak_length) DESC
+
